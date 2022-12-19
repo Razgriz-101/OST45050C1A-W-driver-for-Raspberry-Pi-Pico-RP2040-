77e996ee80c7acc7c14e727f162ee975a4a5ee69
Razgriz-101 <spirit.of.wind.2.3.5.7@gmail.com>
2022-12-19 16:26:26 +0900

    [Add] BMI_cal.py

--- BMI_cal.py
+++ BMI_cal.py
@@ -3,112 +3,124 @@ import tkinter as tk
 judge_list = ['低体重', '痩せすぎ', '痩せ', '痩せぎみ', '普通体重',
               '過体重', '肥満予備軍', '肥満（1度）', '肥満（2度）', '肥満（3度）']
 
-# メインウィンドウ生成
-root = tk.Tk()
-#メインウィンドウのサイズ指定
-#root.geometry("300x200")
-#メインウィンドウの最小サイズを指定
-root.minsize(300, 200)
-#メインウィンドウの背景色
-#root.configure(bg='#ffffff')
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        #メインウィンドウの最小サイズを指定
+        self.minsize(300, 200)
+        # メインウィンドウの設定
+        self.title("BMI計算機")
+        #ウィジェット作成
+        self.create_widgets()
 
-# メインウィンドウの設定
-root.title("BMI計算")
+    def create_widgets(self):
 
-# 3*5のグリッドを設定
-root.columnconfigure(index=0, weight=1)
-root.columnconfigure(index=1, weight=1)
-root.columnconfigure(index=2, weight=3)
-root.rowconfigure(index=0, weight=1)
-root.rowconfigure(index=1, weight=1)
-root.rowconfigure(index=2, weight=1)
-root.rowconfigure(index=3, weight=1)
-root.rowconfigure(index=4, weight=1)
+        # 3*5のグリッドを設定
+        self.columnconfigure(index=0, weight=1)
+        self.columnconfigure(index=1, weight=1)
+        self.columnconfigure(index=2, weight=3)
+        self.rowconfigure(index=0, weight=1)
+        self.rowconfigure(index=1, weight=1)
+        self.rowconfigure(index=2, weight=1)
+        self.rowconfigure(index=3, weight=1)
+        self.rowconfigure(index=4, weight=1)
 
-# ラベルの配置
-text_height = tk.StringVar()
-text_height.set("身長：")
-label = tk.Label(root, textvariable=text_height)
-label.grid(column=0, row=0, sticky='E')
+        # ラベルの配置
+        self.text_height = tk.StringVar()
+        self.text_height.set("身長：")
+        self.label = tk.Label(self.master, textvariable=self.text_height)
+        self.label.grid(column=0, row=0, sticky='E')
 
-text_cm = tk.StringVar()
-text_cm.set('cm')
-label = tk.Label(root, textvariable=text_cm)
-label.grid(column=2, row=0, sticky='W')
+        self.text_cm = tk.StringVar()
+        self.text_cm.set('cm')
+        self.label = tk.Label(self.master, textvariable=self.text_cm)
+        self.label.grid(column=2, row=0, sticky='W')
 
-text = tk.StringVar()
-text.set("体重：")
-label = tk.Label(root, textvariable=text)
-label.grid(column=0, row=1, sticky='E')
+        self.text = tk.StringVar()
+        self.text.set("体重：")
+        self.label = tk.Label(self.master, textvariable=self.text)
+        self.label.grid(column=0, row=1, sticky='E')
 
-text_kg = tk.StringVar()
-text_kg.set('kg')
-label = tk.Label(root, textvariable=text_kg)
-label.grid(column=2, row=1, sticky='W')
+        self.text_kg = tk.StringVar()
+        self.text_kg.set('kg')
+        self.label = tk.Label(self.master, textvariable=self.text_kg)
+        self.label.grid(column=2, row=1, sticky='W')
 
-text_result = tk.StringVar()
-text_result.set('あなたのBMI指数は')
-label = tk.Label(root, textvariable=text_result)
-label.grid(column=0, row=2, sticky='E')
+        self.text_result = tk.StringVar()
+        self.text_result.set('あなたのBMI指数は：')
+        self.label = tk.Label(self.master, textvariable=self.text_result)
+        self.label.grid(column=0, row=2, sticky='E')
 
-bmi_result = tk.DoubleVar()
-bmi_result.set('')
-label = tk.Label(root, textvariable=bmi_result)
-label.grid(column=1, row=2)
+        self.bmi_result = tk.DoubleVar()
+        self.bmi_result.set('')
+        self.label = tk.Label(self.master, textvariable=self.bmi_result)
+        self.label.grid(column=1, row=2)
 
-text_judge = tk.StringVar()
-text_judge.set('判定は')
-label = tk.Label(root, textvariable=text_judge)
-label.grid(column=0, row=3, sticky='E')
+        self.text_judge = tk.StringVar()
+        self.text_judge.set('判定は：')
+        self.label = tk.Label(self.master, textvariable=self.text_judge)
+        self.label.grid(column=0, row=3, sticky='E')
 
-judge = tk.StringVar()
-label = tk.Label(root, textvariable=judge)
-label.grid(column=1, row=3)
+        self.judge = tk.StringVar()
+        self.label = tk.Label(self.master, textvariable=self.judge)
+        self.label.grid(column=1, row=3)
 
-# テキスト入力欄をメインウィンドウに配置
+        # テキスト入力欄をメインウィンドウに配置
+        #身長入力欄
+        self.enter_height = tk.DoubleVar()
+        self.entry = tk.Entry(self.master, textvariable=self.enter_height, bg = 'white', justify="center", width=10)
+        self.entry.grid(column=1, row=0)
+        self.entry.delete(0, tk.END)
 
-#身長入力欄
-enter_height = tk.DoubleVar()
-entry = tk.Entry(root, textvariable=enter_height, bg = 'white', justify="center", width=10)
-entry.grid(column=1, row=0)
-entry.delete(0, tk.END)
+        #体重入力欄
+        self.enter_weight = tk.DoubleVar()
+        self.entry = tk.Entry(self.master, textvariable=self.enter_weight, bg = 'white', justify="center", width=10)
+        self.entry.grid(column=1, row=1)
+        self.entry.delete(0, tk.END)
 
-#体重入力欄
-enter_weight = tk.DoubleVar()
-entry = tk.Entry(root, textvariable=enter_weight, bg = 'white', justify="center", width=10)
-entry.grid(column=1, row=1)
-entry.delete(0, tk.END)
-
-# イベントハンドラ
-def bmi_cal(event):
-    global bmi_result
-    global judge
-    #BMI値を計算
-    bmi_result.set('{:.3f}'.format(enter_weight.get() / (enter_height.get() / 100) ** 2))
-
-    #BMI値によってWHO基準の判定を設定
-    if bmi_result.get() < 16:
-        judge.set(judge_list[0] + '（' + judge_list[1] + '）')
-    elif 16 <= bmi_result.get() < 17:
-        judge.set(judge_list[0] + '（' + judge_list[2] + '）')
-    elif 17 <= bmi_result.get() < 18.5:
-        judge.set(judge_list[0] + '（' + judge_list[3] + '）')
-    elif 18.5 <= bmi_result.get() < 25:
-        judge.set(judge_list[4])
-    elif 25 <= bmi_result.get() < 30:
-        judge.set(judge_list[5] + '（' + judge_list[6] + '）')
-    elif 30 <= bmi_result.get() < 35:
-        judge.set(judge_list[7])
-    elif 35 <= bmi_result.get() < 40:
-        judge.set(judge_list[8])
-    elif 40 <= bmi_result.get():
-        judge.set(judge_list[9])
+        # ボタンウィジェットをメインウィンドウに配置、イベントを登録
+        self.button = tk.Button(self.master, text='計算する', bg = '#ffffff', activebackground = '#dddddd')
+        self.button.bind("<Button-1>", self.bmi_cal)
+        self.button.grid(column=1, row=4)
 
 
-# ボタンウィジェットをメインウィンドウに配置、イベントを登録
-button = tk.Button(root, text='計算する', bg = '#ffffff', activebackground = '#dddddd')
-button.bind("<Button-1>", bmi_cal)
-button.grid(column=1, row=4)
+    # イベントハンドラ
+    def bmi_cal(self, event):
+        try:
+            global bmi_result
+            global judge
+            #BMI値を計算
+            self.bmi_result.set('{:.3f}'.format(self.enter_weight.get() / (self.enter_height.get() / 100) ** 2))
 
-# イベントループの開始
-root.mainloop()
+            # BMI値によってWHO基準の判定を設定
+            if self.bmi_result.get() <= 0:
+                self.bmi_result.set("Error")
+                self.judge.set("入力エラー")
+            elif 0 < self.bmi_result.get() < 16:
+                self.judge.set(judge_list[0] + '（' + judge_list[1] + '）')
+            elif 16 <= self.bmi_result.get() < 17:
+                self.judge.set(judge_list[0] + '（' + judge_list[2] + '）')
+            elif 17 <= self.bmi_result.get() < 18.5:
+                self.judge.set(judge_list[0] + '（' + judge_list[3] + '）')
+            elif 18.5 <= self.bmi_result.get() < 25:
+                self.judge.set(judge_list[4])
+            elif 25 <= self.bmi_result.get() < 30:
+                self.judge.set(judge_list[5] + '（' + judge_list[6] + '）')
+            elif 30 <= self.bmi_result.get() < 35:
+                self.judge.set(judge_list[7])
+            elif 35 <= self.bmi_result.get() < 40:
+                self.judge.set(judge_list[8])
+            elif 40 <= self.bmi_result.get():
+                self.judge.set(judge_list[9])
+
+        except:
+            self.bmi_result.set("Error")
+            self.judge.set("入力エラー")
+
+
+def main():
+    root = App()
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
